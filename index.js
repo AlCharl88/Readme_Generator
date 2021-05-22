2bb4330995c7e574d3a7ddf0631a3c81b8cf52dd
Charly <alctagne@gmail.com>
2021-05-22 02:01:39 -0400

    created an array of questions

--- index.js
+++ index.js
@@ -38,37 +38,87 @@ const questions = [
 
     },
 
-    // Table of Content
-
-    {
-        type: 'input',
-        name: 'Table of Content',
-        message: 'Provide a descriptuon of your project',
-        defaut: 'Project Description',
-        validate: function(answer) {
-            if (answer.length < 1) {
-                return console.log('A description of your project is required')
-            }
-            return true;
-        }
-
-    },
 
     // Installation Instructions
 
     {
         type: 'input',
         name: 'Installation',
-        message: 'Provide a descriptuon of your project',
+        message: 'Provide the installation instructions of your project',
         defaut: 'Installation Instructions',
         validate: function(answer) {
             if (answer.length < 1) {
-                return console.log('Installation instructions of your project is required')
+                return console.log('Installation instructions of your project are required')
             }
             return true;
         }
 
     },
+
+    // Usage Information
+
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide the usage information relative to your project',
+        defaut: 'Usage Information',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log('Usage information relative to your project is required')
+            }
+            return true;
+        }
+
+    },
+
+    // Contribution guidlines
+
+    {
+        type: 'input',
+        name: 'Guidlines',
+        message: 'Provide guidlines for others developers, which can be interrested to contribute to your project',
+        defaut: 'Guidlines',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log('Contribution guidlines to your project are required')
+            }
+            return true;
+        }
+
+    },
+
+    // Text instrcutions
+
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'Provide instruction how to run and test your application',
+        defaut: 'Test Instructions',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log('Test instructions how to run the applications are required')
+            }
+            return true;
+        }
+
+    },
+
+    // License
+    {
+        type: 'input',
+        name: 'License',
+        message: 'Choice a license for your project',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log('A license for your project is required')
+            }
+            return true;
+        }
+
+    },
+
+
 ];
 
 // TODO: Create a function to write README file
